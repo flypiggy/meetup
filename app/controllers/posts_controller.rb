120523e44ae7c64004696d071ce6e98755ec1231
Gavin <gavin.li1986@gmail.com>
2012-12-03 21:55:31 +0800

    allow people create posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,16 @@ class PostsController < ApplicationController
   def show
     @post = Post.find params[:id]
     @comments = @post.comments
+    @comment = Comment.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to posts_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 CourseProject::Application.routes.draw do
   root to: "posts#index"
   match "/post/:id" ,to: "posts#show", as: "post"
+  resources :posts
 end
