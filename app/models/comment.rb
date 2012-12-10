c13961c7fe8adf6d98d1c2c08714fdcbd92980ad
Gavin Li <gavin.li1986@gmail.com>
2012-12-10 23:46:48 +0800

    validate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ActiveRecord::Base
   attr_accessible :content, :post_id
 
   belongs_to :post
+  validates :content, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :title, :url
 
   has_many :comments
+  validates :title, presence: true
+  validates :url, presence: true
 end
