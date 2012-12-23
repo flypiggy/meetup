d15224b5e361e184331c2ce93c8ad19a4ff5d1e6
李甘 <gavin.li1986@gmail.com>
2012-12-23 22:42:56 +0800

    Update config/initializers/twitter.rb

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,3 @@
 Twitter.configure do |config|
-  config.consumer_key = "CZohMBw9RxLdZ53Am13bbQ"
-  config.consumer_secret = "Jqh01XWI3PBBpf93s7vn9olrpjFixlOEoui3aQLc"
-  config.oauth_token = "147608960-lkwxXuMZMZVxpzBNi8qyEwgBUgG33hQtuv6DjF14"
-  config.oauth_token_secret = "gQq781KYgiOdoOG2WITr14tLxXJgYZtbBNecLifwDqM"
+
 end
